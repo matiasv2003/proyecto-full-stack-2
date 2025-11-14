8b19ca24159284585a125a2312e16f175dca3158
Matias Vargas <matia.vargasb@duocuc.cl>
2025-11-14 18:18:43 -0300

    Cambios en Perfil para diseño

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,3 +1,4 @@
+import "./Perfil.css";
 import { useEffect, useState } from "react";
 
 export default function Perfil() {
@@ -5,20 +6,30 @@ export default function Perfil() {
 
   useEffect(() => {
     const userData = JSON.parse(localStorage.getItem("usuario"));
-    console.log("Usuario cargado:", userData); 
+    console.log("Usuario cargado:", userData);
     setUsuario(userData);
   }, []);
 
   if (!usuario) return <h2>No estás logeado.</h2>;
 
   return (
-    <div style={{ padding: "40px", color: "white" }}>
+    <div className="perfil-container">
       <h1>Perfil del Usuario</h1>
 
-      <p><strong>Nombre:</strong> {usuario.nombre}</p>
-      <p><strong>Email:</strong> {usuario.email}</p>
+      <div className="perfil-card">
 
-      <p style={{opacity: 0.7}}>
+        <div className="avatar-box">
+          Foto<br />(140×140)
+        </div>
+
+        <div className="info-box">
+          <p><b>Nombre:</b> {usuario.nombre}</p>
+          <p><b>Email:</b> {usuario.email}</p>
+        </div>
+
+      </div>
+
+      <p className="descripcion">
         (Aquí puedes agregar avatar, historial, configuración, etc.)
       </p>
     </div>
