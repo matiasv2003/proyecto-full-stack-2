3b3c3b3df172e0aef0de33f62a4e658f89d9747d
Matias Vargas <matia.vargasb@duocuc.cl>
2025-11-14 17:58:33 -0300

    Actualizando pagina para mostrar name

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -4,19 +4,17 @@ export default function Perfil() {
   const [usuario, setUsuario] = useState(null);
 
   useEffect(() => {
-    const userData = localStorage.getItem("usuario");
-    if (userData) {
-      setUsuario(JSON.parse(userData));
-    }
+    const userData = JSON.parse(localStorage.getItem("usuario"));
+    console.log("Usuario cargado:", userData); 
+    setUsuario(userData);
   }, []);
 
-  if (!usuario) {
-    return <h2>No estás logeado.</h2>;
-  }
+  if (!usuario) return <h2>No estás logeado.</h2>;
 
   return (
     <div style={{ padding: "40px", color: "white" }}>
       <h1>Perfil del Usuario</h1>
+
       <p><strong>Nombre:</strong> {usuario.nombre}</p>
       <p><strong>Email:</strong> {usuario.email}</p>
 
