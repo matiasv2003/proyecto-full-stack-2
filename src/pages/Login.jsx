08a8d46cc56bb729c50418584a54c4d7ac865648
Matias Vargas <matia.vargasb@duocuc.cl>
2025-11-14 13:45:40 -0300

    Actualizando Puertos del login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -35,7 +35,7 @@ export default function Login() {
 
     // ---- FETCH LOGIN ----
     try {
-      const response = await fetch("http://localhost:8080/api/users/login", {
+        const response = await fetch("http://localhost:8080/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
